d30b99a09b11e1c3838e9884b9b6be402ab9ea14
Timothy Potter <thelabdude@gmail.com>
2020-01-24 12:22:19 -0700

    remove comment

--- update_policy.sh
+++ update_policy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SOLR=http://localhost:8983
-#curl -XPOST "$SOLR/api/cluster/autoscaling" -H "Content-type:application/json" --data-binary @policy.json
+curl -XPOST "$SOLR/api/cluster/autoscaling" -H "Content-type:application/json" --data-binary @policy.json
 curl "$SOLR/api/cluster/autoscaling"
 
 exit 0
