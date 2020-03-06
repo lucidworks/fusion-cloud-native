fd9eb9e03fcbbfafa71fc657d0d8a3ab9747b97e
Kyle Hipke <kyle.hipke@lucidworks.com>
2020-03-05 19:20:01 -0800

    APOLLO-23830: implement OkHttp example, add editorconfig and fix formatting to match our standards

--- jwt-client-example/src/main/java/com/lucidworks/example/jwt/OkHttpClientExample.java
+++ jwt-client-example/src/main/java/com/lucidworks/example/jwt/OkHttpClientExample.java
@@ -2,29 +2,21 @@ package com.lucidworks.example.jwt;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.HttpHeaders;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.apache.http.util.EntityUtils;
+import okhttp3.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.Base64;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Demonstrates how to use JWT authentication to do search queries and indexing against
- * Fusion REST API, using Apache HttpClient.
+ * Fusion REST API, using OkHttp client.
  */
-public class ApacheHttpClient {
-  private static final Logger LOGGER = LoggerFactory.getLogger(ApacheHttpClient.class);
+public class OkHttpClientExample {
+  private static final Logger LOGGER = LoggerFactory.getLogger(OkHttpClientExample.class);
 
   // Object mapper to parse JSON responses from API
   private static final ObjectMapper objectMapper = new ObjectMapper();
@@ -50,50 +42,43 @@ public class ApacheHttpClient {
     String search = System.getProperty("search", "blah+blah");
     String queryUrl = System.getProperty("queryUrl", "/api/apps/" + appId + "/query/" + appId + "?q=" + search);
 
-    // Construct the http client we will use for all calls.
-    final CloseableHttpClient httpClient = HttpClientBuilder.create()
-        // We need this or we get warning logs from Apache HttpClient
-        // about unexpected cookies.
-        .disableCookieManagement()
-        // We need this because this client will be shared between
-        // our jwt refresh thread and our main query thread.
-        // This makes the httpClient thread safe.
-        .setConnectionManager(new PoolingHttpClientConnectionManager())
-        .build();
-
+    // client we will use to make all requests
+    OkHttpClient client = new OkHttpClient();
 
     // Populate our first JWT.
     // This method re-schedules itself to ensure the JWT is refreshed
     // before it expires.
-    refreshJwt(apiUrl, user, password, httpClient);
+    refreshJwt(apiUrl, user, password, client);
 
     LOGGER.info("Querying {}{} every {} milliseconds...", apiUrl, queryUrl, intervalMillis);
     while (true) {
-      executeQuery(apiUrl, queryUrl, httpClient);
+      executeQuery(apiUrl, queryUrl, client);
       Thread.sleep(intervalMillis);
     }
 
 
   }
 
-  private static void executeQuery(String apiUrl, String queryUrl, CloseableHttpClient queryClient) {
+  private static void executeQuery(String apiUrl, String queryUrl, OkHttpClient client) {
     String fullUrl = apiUrl + queryUrl;
-    HttpGet query = new HttpGet(fullUrl);
-    // Authenticate using our current jwt by adding it
-    // in an Authorization header.
-    query.addHeader("Authorization", "Bearer " + jwt);
+    Request request = new Request.Builder()
+        .url(fullUrl)
+        // authenticate using our current jwt by putting it
+        // into an authorization header
+        .addHeader("Authorization", "Bearer " + jwt)
+        .get()
+        .build();
 
     LOGGER.debug("Querying {}", fullUrl);
-    try (CloseableHttpResponse response = queryClient.execute(query)) {
+    try (Response response = client.newCall(request).execute()) {
       // ensure we got a 2xx (ok) response code
-      int statusCode = response.getStatusLine().getStatusCode();
-      if (statusCode < 200 || statusCode > 299) {
+      if (!response.isSuccessful()) {
         LOGGER.error("Query failed: received non-2xx response {} from" +
-            " Fusion REST API. Exiting...", statusCode);
+            " Fusion REST API. Exiting...", response.code());
         //check for an entity and serialize it if there was one to make
         //the error message more informative
-        if (response.getEntity() != null) {
-          LOGGER.error("Error response body was: {}", EntityUtils.toString(response.getEntity()));
+        if (response.body() != null) {
+          LOGGER.error("Error response body was: {}", response.body().string());
         }
         System.exit(-1);
       }
@@ -107,35 +92,33 @@ public class ApacheHttpClient {
    * Refreshes the current JWT by obtaining a new one from the Fusion REST API and
    * schedules this method to run again before the JWT expires.
    */
-  private static void refreshJwt(String apiUrl, String user, String password, CloseableHttpClient queryClient) {
+  private static void refreshJwt(String apiUrl, String user, String password, OkHttpClient client) {
     String loginUrl = apiUrl + "/oauth2/token";
-    HttpPost jwtRequest = new HttpPost(loginUrl);
-    // add the basic authorization header that this endpoint requires.
-    String auth = user + ":" + password;
-    String encodedAuth = Base64.getEncoder().encodeToString((auth).getBytes());
-    String authHeader = "Basic " + new String(encodedAuth);
-    jwtRequest.setHeader(HttpHeaders.AUTHORIZATION, authHeader);
+
+    Request jwtRequest = new Request.Builder()
+        .url(loginUrl)
+        // add the basic authorization header that this endpoint requires
+        .addHeader("Authorization", Credentials.basic(user, password))
+        .post(RequestBody.create("", MediaType.get("application/json")))
+        .build();
 
 
     // Execute the HttpPost to get the JWT
     LOGGER.info("Obtaining new JWT via {}", loginUrl);
-    try (CloseableHttpResponse response = queryClient.execute(jwtRequest)) {
-
+    try (Response response = client.newCall(jwtRequest).execute()) {
       // ensure we got a 2xx (ok) response code
-      int statusCode = response.getStatusLine().getStatusCode();
-      if (statusCode < 200 || statusCode > 299) {
+      if (!response.isSuccessful()) {
         LOGGER.error("Attempt to retrieve JWT token failed: received non-2xx response {} from" +
-            " Fusion REST API. Exiting...", statusCode);
-        //check for an entity and serialize it if there was one to make
-        //the error message more informative
-        if (response.getEntity() != null) {
-          LOGGER.error("Error response body was: {}", EntityUtils.toString(response.getEntity()));
+            " Fusion REST API. Exiting...", response.code());
+        // log the error response if we got one
+        if (response.body() != null) {
+          LOGGER.error("Error response body was: {}", response.body().string());
         }
         System.exit(-1);
       }
 
       // response code was okay, retrieve the JWT from the body
-      JsonNode responseJSON = objectMapper.readTree(response.getEntity().getContent());
+      JsonNode responseJSON = objectMapper.readTree(response.body().byteStream());
       jwt = responseJSON.get("access_token").asText();
       long secondsUntilExpiration = responseJSON.get("expires_in").longValue();
 
@@ -150,7 +133,7 @@ public class ApacheHttpClient {
       LOGGER.info("Successfully refreshed JWT, refreshing again in {} seconds", secondsUntilRefresh);
 
       // schedule it to be refreshed (by calling this method again) before it expires
-      refreshTokenExecutor.schedule(() -> refreshJwt(apiUrl, user, password, queryClient), secondsUntilRefresh, TimeUnit.SECONDS);
+      refreshTokenExecutor.schedule(() -> refreshJwt(apiUrl, user, password, client), secondsUntilRefresh, TimeUnit.SECONDS);
 
     } catch (IOException e) {
       LOGGER.error("Attempt to retrieve JWT token failed due to exception. Exiting...", e);
