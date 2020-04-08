0bc30aab6e73ee7a5309d685fd31bfd3ebe34dc8
luis-munoz <lmunozes@gmail.com>
2020-04-08 12:46:52 -0500

    fixed retry block

--- setup_f5_k8s.sh
+++ setup_f5_k8s.sh
@@ -427,35 +427,25 @@ if [ "$UPGRADE" != "1" ]; then
     num_nodes=1
     if [ "${NODE_POOL}" != "" ] && [ "${NODE_POOL}" != "{}" ]; then
       node_selector=$(tr ': ' '=' <<<"${NODE_POOL}")
-      find_nodes=$(kubectl get nodes -l "${node_selector}" | grep -i ready)
-      has_nodes=$?
-      if [ "${has_nodes}" == "0" ]; then
-        echo -e "Found at least one healthy node matching nodeSelector: ${NODE_POOL}"
-        num_nodes=$(kubectl get nodes -l "${node_selector}" | grep -i ready | wc -l)
-      else
-        echo -e "\nERROR: No 'Ready' nodes found matching nodeSelector: ${node_selector}! Retrying in 30 seconds"
-        echo -ne "."
-        sleep 30
-        #Adding a retry loop because EKS takes more time to create nodes.
-        retries=2
-        while (( retries > 0 )); do
-          find_nodes=$(kubectl get nodes -l "${node_selector}" | grep -i ready)
-          has_nodes=$?
-          if [ "${has_nodes}" == "0" ]; then
-            echo -e "Found at least one healthy node matching nodeSelector: ${NODE_POOL}"
-            num_nodes=$(kubectl get nodes -l "${node_selector}" | grep -i ready | wc -l)
-            retries=0
-          else
-            echo -e "\nERROR: No 'Ready' nodes found matching nodeSelector: ${node_selector}! Retrying in 30 seconds"
-            retries=$(( retries - 1 ))
-          fi
-
-        done
-        if [ "${has_nodes}" != "0" ]; then
-          echo -e "\nERROR: No 'Ready' nodes found matching nodeSelector: ${node_selector}! Check the '--node-pool' parameter and retry running this script!\n"
-          kubectl get nodes -l "${node_selector}"
-          exit 1
+      #Adding a retry loop because EKS takes more time to create nodes.
+      retries=2
+      while (( retries > 0 )); do
+        find_nodes=$(kubectl get nodes -l "${node_selector}" | grep -i ready)
+        has_nodes=$?
+        if [ "${has_nodes}" == "0" ]; then
+          echo -e "Found at least one healthy node matching nodeSelector: ${NODE_POOL}"
+          num_nodes=$(kubectl get nodes -l "${node_selector}" | grep -i ready | wc -l)
+          retries=-1
+        else
+          echo -e "\nERROR: No 'Ready' nodes found matching nodeSelector: ${node_selector}! Retrying in 30 seconds"
+          echo -ne "."
+          sleep 30
+          retries=$(( retries - 1 ))
         fi
+      done
+      if [ "${retries}" == "0" ]; then
+        echo -e "\nERROR: No 'Ready' nodes found matching nodeSelector: ${node_selector}! Check the '--node-pool' parameter and retry running this script!\n"
+        exit 1
       fi
     else
       num_nodes=$(kubectl get nodes | grep -i ready | wc -l)
