25e114181a3fa082ef23766368f3f577b3fc898c
apurvamishra <apurva.mishra@lucidworks.com>
2020-12-23 15:38:28 +0530

    APOLLO-28288 updated with review comments

--- update_signal_collection.sh
+++ update_signal_collection.sh
@@ -1,3 +1,3 @@
 PROXY="http://localhost:6764"
 APP="YOUR_FUSION_APP_ID"
-curl -u $CREDS -X POST "$PROXY/api/update/all/signalCollection" > ${APP}_signal_collection_update.json
+curl -u $CREDS -X POST "$PROXY/api/update/all/signalCollection"
