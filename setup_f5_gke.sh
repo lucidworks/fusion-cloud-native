a48962dc618392632b832d5f26861aa32967f21c
Luis Munoz <lmunozes@gmail.com>
2023-06-21 18:18:06 -0500

    Release 5.9.0

--- setup_f5_gke.sh
+++ setup_f5_gke.sh
@@ -2,8 +2,8 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 
 INSTANCE_TYPE=""
-CHART_VERSION="5.8.1"
-GKE_MASTER_VERSION="1.24.12-gke.1000"
+CHART_VERSION="5.9.0"
+GKE_MASTER_VERSION="1.26.3-gke.1000"
 NODE_POOL="cloud.google.com/gke-nodepool: default-pool"
 PROMETHEUS="install"
 SCRIPT_CMD="$0"
