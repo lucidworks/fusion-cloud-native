9106dc931eb8acec0dd42657dd341349c13baa48
Andre Smart <andre.smart@lucidworks.com>
2025-02-20 16:55:37 -0500

    Updates release notes for 5.9.10

--- setup_f5_gke.sh
+++ setup_f5_gke.sh
@@ -2,7 +2,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 
 INSTANCE_TYPE=""
-CHART_VERSION="5.9.9"
+CHART_VERSION="5.9.10"
 GKE_MASTER_VERSION="1.29.4-gke.1043002"
 NODE_POOL="cloud.google.com/gke-nodepool: default-pool"
 PROMETHEUS="install"
